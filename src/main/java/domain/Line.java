e19ee798837d4fafe605783e8aecb253eca40f79
swonny <wonny921@gmail.com>
2023-12-19 21:37:33 +0900

    feat: 현재 위치를 입력 받아 이동 가능한 위치를 반환하는 기능 구현

--- src/main/java/domain/Line.java
+++ src/main/java/domain/Line.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 public class Line {
 
+    private static final int START_POINT = 0;
+
     private final List<Boolean> bars;
 
     public Line(final List<Boolean> bars) {
@@ -23,6 +25,66 @@ public class Line {
         if (previous && current) {
             throw new IllegalArgumentException("사다리는 연속적으로 존재할 수 없습니다.");
         }
+    }
+
+    // TODO: 2023/12/19 move로직 리팩터링 필요
+    public int move(final Position position) {
+        final int currentX = position.getX();
+        if (isLeftMovable(currentX)) {
+            return calculateLeft(currentX);
+        }
+        if (isRightMovable(currentX)) {
+            return calculateRight(currentX);
+        }
+        return currentX;
+    }
+
+    private boolean isLeftMovable(final int current) {
+        if (canMoveLeft(current)) {
+            final int left = calculateLeft(current);
+            return isConnected(left, current);
+        }
+        return false;
+    }
+
+    private boolean canMoveLeft(final int current) {
+        return current > START_POINT;
+    }
+
+    private int calculateLeft(final int currentX) {
+        if (currentX == START_POINT) {
+            return currentX;
+        }
+        return currentX - 1;
+    }
+
+    private boolean isConnected(final int currentX, final int movablePosition) {
+        if (currentX == movablePosition) {
+            return false;
+        }
+        if (currentX < movablePosition) {
+            return bars.get(currentX);
+        }
+        return bars.get(currentX - 1);
+    }
+
+    private boolean isRightMovable(final int current) {
+        if (canMoveRight(current)) {
+            final int right = calculateRight(current);
+            return isConnected(right, current);
+        }
+        return false;
+    }
+
+    private boolean canMoveRight(final int current) {
+        return current < bars.size();
+    }
+
+    private int calculateRight(final int currentX) {
+        if (currentX > bars.size() - 1) {
+            return currentX;
+        }
+        return currentX + 1;
     }
 
     public List<Boolean> getBars() {
--- src/test/java/domain/LineTest.java
+++ src/test/java/domain/LineTest.java
@@ -1,9 +1,12 @@
 package domain;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LineTest {
@@ -16,5 +19,19 @@ class LineTest {
         // when & then
         assertThatThrownBy(() -> new Line(bars))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"0:1", "1:0", "2:3", "3:2"}, delimiter = ':')
+    void 위치를_입력받은_경우_이동할_수_있는_위치로_이동한_위치를_반환한다_2(final int x, final int expected) {
+        // given
+        final Line line = new Line(List.of(true, false, true));
+        final Position currentPosition = new Position(x, 0);
+
+        // when
+        final int movedX = line.move(currentPosition);
+
+        // then
+        assertThat(movedX).isEqualTo(expected);
     }
 }
