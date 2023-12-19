f70bde4f0f1c9be079e1ba353bf0a1679ebed6b7
swonny <wonny921@gmail.com>
2023-12-19 17:43:00 +0900

    fix: 불필요하게 생성되는 사다리 칸 줄이기

--- src/main/java/domain/LineFactory.java
+++ src/main/java/domain/LineFactory.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 public class LineFactory {
 
+    private static final int SUBTRACT_COUNT = 1;
+
     public static Line from(final BarGenerateStrategy generateStrategy, final int participantCount) {
-        final List<Boolean> bars = generateStrategy.generate(participantCount);
+        final int barCount = participantCount - SUBTRACT_COUNT;
+        final List<Boolean> bars = generateStrategy.generate(barCount);
 
         return new Line(bars);
     }
